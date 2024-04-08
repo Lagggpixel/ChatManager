2ae851e5ebf8a3782b254dae962171c4c125c13b
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-08 01:15:35 -0400

    Remove static creation

--- paper/src/main/java/me/h1dd3nxn1nja/chatmanager/ChatManager.java
+++ paper/src/main/java/me/h1dd3nxn1nja/chatmanager/ChatManager.java
@@ -1,26 +1,17 @@
 package me.h1dd3nxn1nja.chatmanager;
 
 import me.h1dd3nxn1nja.chatmanager.listeners.ChatListener;
-import me.h1dd3nxn1nja.chatmanager.renderers.ChatRenderer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChatManager extends JavaPlugin {
 
-    private static ChatRenderer chatRenderer;
-
     @Override
     public void onEnable() {
-        chatRenderer = new ChatRenderer();
-
         getServer().getPluginManager().registerEvents(new ChatListener(), this);
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
-    }
-
-    public static ChatRenderer getChatRenderer() {
-        return chatRenderer;
     }
 }
